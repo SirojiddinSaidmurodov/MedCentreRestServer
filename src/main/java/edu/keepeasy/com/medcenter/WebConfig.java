659b9de4f9de7224e9c9470cc84909cea9913f35
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-04-03 22:29:27 +0300

    fix CORS
    Took 11 minutes

---
+++ src/main/java/edu/keepeasy/com/medcenter/WebConfig.java
@@ -0,0 +1,18 @@
+package edu.keepeasy.com.medcenter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "PUT", "DELETE", "POST")
+                .allowedOrigins("http://localhost:8081");
+    }
+}
