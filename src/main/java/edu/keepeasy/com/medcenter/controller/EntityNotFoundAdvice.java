ab13f5fc62726063453cf8ea361ad3d7dd89a3fb
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-04-03 20:53:41 +0300

    add 404 error handlers

--- src/main/java/edu/keepeasy/com/medcenter/controller/EntityNotFoundAdvice.java
+++ src/main/java/edu/keepeasy/com/medcenter/controller/EntityNotFoundAdvice.java
@@ -1,7 +1,9 @@
 package edu.keepeasy.com.medcenter.controller;
 
 import edu.keepeasy.com.medcenter.exceptions.DepartmentNotFoundException;
+import edu.keepeasy.com.medcenter.exceptions.DoctorNotFoundException;
 import edu.keepeasy.com.medcenter.exceptions.PatientNotFoundException;
+import edu.keepeasy.com.medcenter.exceptions.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class PatientNotFoundAdvice {
+public class EntityNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(PatientNotFoundException.class)
@@ -22,6 +24,20 @@ public class PatientNotFoundAdvice {
     @ExceptionHandler(DepartmentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String departmentNotFoundHandler(DepartmentNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(DoctorNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String doctorNotFoundHandler(DoctorNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String recordNotFoundHandler(RecordNotFoundException e) {
         return e.getMessage();
     }
 }
